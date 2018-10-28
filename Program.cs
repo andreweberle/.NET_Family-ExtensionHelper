c6953587f1c2a23a6f2ecf86f02bdfbc1b47d1a2
Andrew Eberle <andreweberle@outlook.com>
2018-10-28 13:00:36 +1100

    Added Latest DLL
    Added Lastest DLL.

--- Program.cs
+++ Program.cs
@@ -8,16 +8,12 @@ namespace MyApplication
     class Program
     {
         static void Main()
-        {
-            Console.WriteLine("Hello World".PrintColouredText(ConsoleColor.Green));
-
-            
-
+        {           
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             string fileName = "foo.txt";
             string fullPath = System.IO.Path.Combine(desktopPath,fileName);
 
-            fullPath.WatchPath("*.*");
+            Console.WriteLine(fullPath.IsFileLocked());
         }
     }
 }
