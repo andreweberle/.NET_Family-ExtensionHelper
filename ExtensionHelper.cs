6794d9cb862e943fb1db65f4cc7628a98cb77243
Andrew Eberle <andreweberle@outlook.com>
2018-11-05 11:59:42 +1100

    Stream To Byte Array Added
    Added Stream To Byte Array Thanks to Jon Skeet.

--- ExtensionHelper.cs
+++ ExtensionHelper.cs
@@ -1024,5 +1024,24 @@ namespace EbbsSoft
         {
             return ValidateLuhn(Convert.ToString(sum));
         }
+
+        /// <summary>
+        /// Convert Stream To Byte Array
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static byte[] StreamToByteArray(this Stream stream)
+        {
+            byte[] buffer = new byte[16*1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = stream.Read(buffer,0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
     }
 }
