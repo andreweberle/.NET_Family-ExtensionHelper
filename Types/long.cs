e3fef7e215af6b9a44f92b04f7647c5410f44fbb
Andrew Eberle <andreweberle@outlook.com>
2019-05-26 16:45:24 +1000

    Calculate Directory Size.

---
+++ Types/long.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using EbbsSoft.ExtensionHelpers.StringHelpers;
+
+namespace EbbsSoft.ExtensionHelpers.LongHelpers
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Calculate Directory Size.
+        /// </summary>
+        /// <param name="dirInfo"></param>
+        /// <returns></returns>
+        public static long DirectorySize(this DirectoryInfo dirInfo)
+        {
+            long size = 0;
+            FileInfo[] fileIndo = dirInfo.GetFiles();
+            foreach (var file in fileIndo)
+            {
+                size += file.Length;
+            }
+            DirectoryInfo[] subDirInfo = dirInfo.GetDirectories();
+            foreach (var subDir in subDirInfo)
+            {
+                size += subDir.DirectorySize();
+            }
+
+            return size;
+        }
+    }
+}
